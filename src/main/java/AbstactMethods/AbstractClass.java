e579f99f76df5b3526625b8fbd11731abce3990a
pavan kumar p <pamartipk@gmail.com>
2026-05-11 21:27:23 +0530

    Abstaract class updadte

--- src/main/java/AbstactMethods/AbstractClass.java
+++ src/main/java/AbstactMethods/AbstractClass.java
@@ -1,9 +1,20 @@
 package AbstactMethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AbstractClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+WebDriver driver = new ChromeDriver();
+		
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.hyrtutorials.com/p/frames-practice.html");
+		
 
 	}
 
